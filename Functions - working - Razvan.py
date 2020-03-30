e38ea70e54bfab846f4f9749c22d9fe421d99862
zoldirazvan <62897785+zoldirazvan@users.noreply.github.com>
2020-03-31 01:21:56 +0300

    Update Functions - working - Razvan.py

--- Functions - working - Razvan.py
+++ Functions - working - Razvan.py
@@ -12,11 +12,47 @@ description = ['Country', [
 ]]
 
 # reduce raw data to have a clear image how everything is working
+
 raw_data = [
     ('AL', [': ', ': ', ': ', ': ', ': ', ': ', ': ', '84 ', ': ']),
     ('AT', ['75 ', '79 ', '81 ', '81 ', '82 ', '85 ', '89 ', '89 ', '90 ']),
-    ('BA', [': ', ': ', ': ', ': ', ': ', ': ', ': ', '69 ', '72 '])
-
+    ('BA', [': ', ': ', ': ', ': ', ': ', ': ', ': ', '69 ', '72 ']),
+    ('BE', ['77 ', '78 ', '80 ', '83 ', '82 ', '85 ', '86 ', '87 ', '90 ']),
+    ('BG', ['45 ', '51 ', '54 ', '57 ', '59 ', '64 ', '67 ', '72 ', '75 ']),
+    ('CH', [': ', ': ', ': ', '91 ', ': ', ': ', '93 b', ': ', '96 ']),
+    ('CY', ['57 ', '62 ', '65 ', '69 ', '71 ', '74 ', '79 ', '86 ', '90 ']),
+    ('CZ', ['67 ', '73 ', '73 ', '78 ', '79 ', '82 ', '83 ', '86 ', '87 ']),
+    ('DE', ['83 ', '85 ', '88 ', '89 ', '90 ', '92 ', '93 ', '94 ', '95 ']),
+    ('DK', ['90 ', '92 ', '93 ', '93 ', '92 ', '94 ', '97 ', '93 ', '95 ']),
+    ('EA', ['74 ', '76 ', '79 ', '81 ', '83 ', '85 ', '87 ', '89 ', '90 ']),
+    ('EE', ['69 ', '74 ', '79 ', '83 ', '88 ', '86 ', '88 ', '90 ', '90 ']),
+    ('EL', ['50 ', '54 ', '56 ', '66 ', '68 ', '69 ', '71 ', '76 ', '79 ']),
+    ('ES', ['63 ', '67 ', '70 ', '74 ', '79 ', '82 ', '83 ', '86 ', '91 ']),
+    ('FI', ['84 ', '87 ', '89 ', '90 ', '90 ', '92 ', '94 ', '94 ', '94 ']),
+    ('FR', ['76 ', '80 ', '82 ', '83 ', '83 ', '86 ', '86 ', '89 ', '90 ']),
+    ('HR', ['61 ', '66 ', '65 ', '68 ', '77 ', '77 ', '76 ', '82 ', '81 ']),
+    ('HU', ['63 ', '67 ', '70 ', '73 ', '76 ', '79 ', '82 ', '83 ', '86 ']),
+    ('IE', ['78 ', '81 ', '82 ', '82 ', '85 ', '87 ', '88 ', '89 ', '91 ']),
+    ('IS', ['93 ', '95 ', '96 ', '96 ', ': ', ': ', '98 ', '99 ', '98 ']),
+    ('IT', ['62 ', '63 ', '69 ', '73 ', '75 ', '79 ', '81 ', '84 ', '85 ']),
+    ('LT', ['60 ', '60 ', '65 ', '66 ', '68 ', '72 ', '75 ', '78 ', '82 ']),
+    ('LU', ['91 ', '93 ', '94 ', '96 ', '97 ', '97 ', '97 ', '93 b', '95 ']),
+    ('LV', ['64 ', '69 ', '72 ', '73 ', '76 ', '77 b', '79 ', '82 ', '85 ']),
+    ('ME', [': ', '55 ', ': ', ': ', ': ', ': ', '71 ', '72 ', '74 ']),
+    ('MK', [': ', '58 ', '65 ', '68 ', '69 ', '75 ', '74 ', '79 ', '82 ']),
+    ('MT', ['75 ', '77 ', '78 ', '80 ', '81 ', '81 ', '85 ', '84 ', '86 ']),
+    ('NL', ['94 ', '94 ', '95 ', '96 ', '96 ', '97 ', '98 ', '98 ', '98 ']),
+    ('NO', ['92 ', '93 ', '94 ', '93 ', '97 ', '97 ', '97 ', '96 ', '98 ']),
+    ('PL', ['67 ', '70 ', '72 ', '75 ', '76 ', '80 ', '82 ', '84 ', '87 ']),
+    ('PT', ['58 ', '61 ', '62 ', '65 ', '70 ', '74 ', '77 ', '79 ', '81 ']),
+    ('RO', ['47 ', '54 ', '58 ', '61 b', '68 ', '72 ', '76 ', '81 ', '84 ']),
+    ('RS', [': ', ': ', ': ', ': ', '64 ', ': ', '68 ', '73 ', '80 ']),
+    ('SE', ['91 ', '92 ', '93 ', '90 ', '91 ', '94 b', '95 ', '93 ', '96 ']),
+    ('SI', ['73 ', '74 ', '76 ', '77 ', '78 ', '78 ', '82 ', '87 ', '89 ']),
+    ('SK', ['71 ', '75 ', '78 ', '78 ', '79 ', '81 ', '81 ', '81 ', '82 ']),
+    ('TR', [': ', '47 ', '49 ', '60 ', '70 ', '76 ', '81 ', '84 ', '88 ']),
+    ('UK', ['83 ', '87 ', '88 ', '90 ', '91 ', '93 ', '94 ', '95 ', '96 ']),
+    ('XK', [': ', ': ', ': ', ': ', ': ', ': ', '89 ', '93 ', '93 ']),
 ]
 
 type_average_list = ['Year', 'Country']
@@ -49,7 +85,7 @@ def retrieve_data_for_year(year):
     for item1 in dataset1.keys():
         list_year.append((item1, dataset1[item1][year]))
         len_list_year = len(list_year)
-    return [f'Data for {year} is: {list_year}'], [len_list_year]
+    return [f'Data for {year} is: {list_year}']
 
 
 # function to retrieve data for each country
@@ -58,48 +94,37 @@ def retrieve_data_for_year(year):
 def retrieve_data_for_country(country):
     global len_list_country, dataset1, list_country
     for item2 in dataset1[country]:
-        list_country.append((item2, dataset1[country][year]))
+        list_country.append((item2, dataset1[country][item2]))
         len_list_country = len(list_country)
-    return [f'Data for {country} is: {list(list_country)}'], [len_list_country]
+    return [f'Data for {country} is: {list(list_country)}']
+
 
 # function to perform average from an iterable(of year data or country data)
 # This function will take an iterable as an argument
-
-def QuestionOne():
-    # check (search) if some item is in list; let user choose what is searching for
-    print('Do you want to perform average by : ')
-    for number, item in enumerate(type_average_list):
-        print(f'{number + 1}: {item}')
-
-
+# Victor's idea !!!
 def perform_average():
     global list_year, len_list_year, list_country, len_list_country
-    QuestionOne()
-    question_one_input = int(input())
-    value = 0
-    if question_one_input == 1:
-        for (item, value) in list_year:
-            if value != ': ' and int(value) > 0:
-                value += int(value[:-1])
-                value2 = value / len_list_year
-    elif question_one_input == 2:
-        for (item, value) in list_country:
-            if value != ': ' and int(value) > 0:
-                value += int(value[:-1])
-                value2 = value / len_list_country
-    return value2
+    total = counter = 0
+    for (item, value) in list_year:
+        if value != ': ' and int(value[:-1]) > 0:
+            total += int(value[:-1])
+            counter += 1
+    return total / counter
+
 
 datas = listToDict(raw_data)
 print(datas)
 print()
+
 year = '2018 '
-country = 'AL'
+country = 'AT'
 data_for_year = retrieve_data_for_year(year)
 print(data_for_year)
 print(len_list_year)
 print()
+
 data_for_country = retrieve_data_for_country(country)
 print(data_for_country)
 print(len_list_country)
 print()
-perform_average()
+print(perform_average())
