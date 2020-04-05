6d0be0fec5e6e620ebc442dace272fb654dc8219
Mihai <farcasmmihai@gmail.com>
2020-04-05 17:03:48 +0300

    some changes in code

--- homework5_2+.py
+++ homework5_2+.py
@@ -1,23 +1,7 @@
-#!Chalange 2!:
+from pprint import pprint
 
-# Transform the map and lambda example using list comprehensions.
-
-# lambda function used inside map built-in
-# lst = [2, 4, 6, 8, 10]
-# squared_lst = list(map(lambda x: x**2, lst))
-# print(squared_lst)
-
-# Starting with lst = [2, 4, 6, 8, 10] we need optain the resoult squared_lst = [4, 16, 36, 64, 100]
-
-lst = [2, 4, 6, 8, 10]
-squared_lst = [x ** 2 for x in lst[::1]]
-print(squared_lst)
-print('-------------')
-
-#
 description = ('Country', [
-    '2011 ', '2012 ', '2013 ', '2014 ', '2015 ', '2016 ', '2017 ', '2018 ',
-    '2019 '
+    '2011 ', '2012 ', '2013 ', '2014 ', '2015 ', '2016 ', '2017 ', '2018 ', '2019 '
 ])
 
 raw_data = [
@@ -26,7 +10,7 @@ raw_data = [
     ('BA', [': ', ': ', ': ', ': ', ': ', ': ', ': ', '69 ', '72 ']),
     ('BE', ['77 ', '78 ', '80 ', '83 ', '82 ', '85 ', '86 ', '87 ', '90 ']),
     ('BG', ['45 ', '51 ', '54 ', '57 ', '59 ', '64 ', '67 ', '72 ', '75 ']),
-    ('CH', [': ', ': ', ': ', '91 ', ': ', ': ', '93 b', ': ', '96 ']),
+    ('CH', [': ', ': ', ': ', '91 ', ': ', ': ', '93', ': ', '96 ']),
     ('CY', ['57 ', '62 ', '65 ', '69 ', '71 ', '74 ', '79 ', '86 ', '90 ']),
     ('CZ', ['67 ', '73 ', '73 ', '78 ', '79 ', '82 ', '83 ', '86 ', '87 ']),
     ('DE', ['83 ', '85 ', '88 ', '89 ', '90 ', '92 ', '93 ', '94 ', '95 ']),
@@ -44,7 +28,7 @@ raw_data = [
     ('IT', ['62 ', '63 ', '69 ', '73 ', '75 ', '79 ', '81 ', '84 ', '85 ']),
     ('LT', ['60 ', '60 ', '65 ', '66 ', '68 ', '72 ', '75 ', '78 ', '82 ']),
     ('LU', ['91 ', '93 ', '94 ', '96 ', '97 ', '97 ', '97 ', '93 b', '95 ']),
-    ('LV', ['64 ', '69 ', '72 ', '73 ', '76 ', '77 b', '79 ', '82 ', '85 ']),
+    ('LV', ['64 ', '69 ', '72 ', '73 ', '76 ', '77 ', '79 ', '82 ', '85 ']),
     ('ME', [': ', '55 ', ': ', ': ', ': ', ': ', '71 ', '72 ', '74 ']),
     ('MK', [': ', '58 ', '65 ', '68 ', '69 ', '75 ', '74 ', '79 ', '82 ']),
     ('MT', ['75 ', '77 ', '78 ', '80 ', '81 ', '81 ', '85 ', '84 ', '86 ']),
@@ -52,9 +36,9 @@ raw_data = [
     ('NO', ['92 ', '93 ', '94 ', '93 ', '97 ', '97 ', '97 ', '96 ', '98 ']),
     ('PL', ['67 ', '70 ', '72 ', '75 ', '76 ', '80 ', '82 ', '84 ', '87 ']),
     ('PT', ['58 ', '61 ', '62 ', '65 ', '70 ', '74 ', '77 ', '79 ', '81 ']),
-    ('RO', ['47 ', '54 ', '58 ', '61 b', '68 ', '72 ', '76 ', '81 ', '84 ']),
+    ('RO', ['47 ', '54 ', '58 ', '61 ', '68 ', '72 ', '76 ', '81 ', '84 ']),
     ('RS', [': ', ': ', ': ', ': ', '64 ', ': ', '68 ', '73 ', '80 ']),
-    ('SE', ['91 ', '92 ', '93 ', '90 ', '91 ', '94 b', '95 ', '93 ', '96 ']),
+    ('SE', ['91 ', '92 ', '93 ', '90 ', '91 ', '94', '95 ', '93 ', '96 ']),
     ('SI', ['73 ', '74 ', '76 ', '77 ', '78 ', '78 ', '82 ', '87 ', '89 ']),
     ('SK', ['71 ', '75 ', '78 ', '78 ', '79 ', '81 ', '81 ', '81 ', '82 ']),
     ('TR', [': ', '47 ', '49 ', '60 ', '70 ', '76 ', '81 ', '84 ', '88 ']),
@@ -62,53 +46,57 @@ raw_data = [
     ('XK', [': ', ': ', ': ', ': ', ': ', ': ', '89 ', '93 ', '93 ']),
 ]
 
-dataset = {'Romania': [{'year': '2019', 'coverage': 84},
-                       {'year': '2018', 'coverage': 81},
-                       {'year': '2017', 'coverage': 76},
-                       {'year': '2016', 'coverage': 72},
-                       {'year': '2015', 'coverage': 68},
-                       {'year': '2014', 'coverage': 61},
-                       {'year': '2013', 'coverage': 58},
-                       {'year': '2012', 'coverage': 54},
-                       {'year': '2011', 'coverage': 47},
-                       ], 'Germany': [{'year': '2019', 'coverage': 95},
-                                      {'year': '2018', 'coverage': 94},
-                                      {'year': '2017', 'coverage': 93},
-                                      {'year': '2016', 'coverage': 92},
-                                      {'year': '2015', 'coverage': 90},
-                                      {'year': '2014', 'coverage': 89},
-                                      {'year': '2013', 'coverage': 88},
-                                      {'year': '2012', 'coverage': 85},
-                                      {'year': '2011', 'coverage': 83},
-                                      ]}
 
-print(dataset.keys())
-print(dataset.values())
-print('============================================================')
+def process_data(data_to_process):
+    years = description[1]
+    dataset_data_to_process = {
+        country: [
+            {'year': year.strip(), 'coverage': coverage.strip() if coverage != ": " else "0"}
+            for coverage, year in zip(coverages, years)
+        ]
+        for country, coverages in data_to_process
+    }
+
+    pprint(dataset_data_to_process)
+    return dataset_data_to_process
 
 
-# get_country_data(dataset,country)
-def get_country_data(dataset, country):
-    for i in range(len(dataset[(f"{country}")])):
+def get_country_data(dataset_get_country_data, country):
+    for i in range(len(dataset_get_country_data[f"{country}"])):
         dict_get_country_data = {
-            country: [(dataset[(f"{country}")][i]['year']), (dataset[(f"{country}")][i]['coverage'])]
-            for year, coverage in (dataset[(f"{country}")])
+            country: [(dataset_get_country_data[(f"{country}")][i]['year']), (dataset_get_country_data[(f"{country}")][i]['coverage'])]
+            for year, coverage in (dataset_get_country_data[f"{country}"])
         }
         print(dict_get_country_data)
 
-    # =================================================================
 
-
-# perform average
-country_data = get_country_data(dataset, "Romania")
-
-
-def perform_average(dataset, country):
+def perform_average(dataset_country, country):
     c = 0
-    for i in range(len(dataset[(f"{country}")])):
-        c += (dataset[(f"{country}")][i]['coverage'])
+    d = 0
+    for i in range(len(dataset_country[f"{country}"])):
+        if int((dataset_country[f"{country}"][i]['coverage'])) != "0":
+            c += int((dataset_country[f"{country}"][i]['coverage']))
+            d += 1
     print('\n====================average===============')
-    print(round(float(c / (len(dataset[(f"{country}")]))), 2))
+    print(round(float(c / d), 2))
 
 
-perform_average(dataset, 'Romania')
+def get_year_data(dataset_get_year_data, input_year):
+    data = {
+        input_year: [
+            (country, element["coverage"])
+            for country, countryData in dataset_get_year_data.items()
+            for element in countryData
+            if element["year"] == input_year
+        ]
+    }
+
+    pprint(data)
+
+    return data
+
+
+dataset = process_data(raw_data)
+get_country_data(dataset, 'AL')
+get_year_data(dataset, '2012')
+perform_average(dataset, 'RS')
