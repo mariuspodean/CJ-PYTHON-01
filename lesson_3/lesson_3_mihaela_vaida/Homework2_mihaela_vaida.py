80f1758de7e674bb1c0c8c3e14b5f178e46f4d87
mihaelaGit <radoi_mihaela86@yahoo.com>
2020-04-09 10:47:20 +0300

    Lesson3

---
+++ lesson_3/lesson_3_mihaela_vaida/Homework2_mihaela_vaida.py
@@ -0,0 +1,9 @@
+#Append challenge: Mimic the behaviour of append() by using insert().
+#Write a code snippet with the answer.
+
+fruits=['apple', 'apple', 'banana', 'banana', 'kiwi', 'orange', 'pear']
+fruits.append('dragon fruit')
+fruits.insert(len(fruits),'avocado')
+fruits
+#['apple', 'apple', 'banana', 'banana', 'kiwi', 'orange', 'pear', 'dragon fruit', 'avocado']
+
