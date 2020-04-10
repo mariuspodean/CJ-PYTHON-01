0d3185a24c3b322e0b9b200ae7b14ab9657b62ee
Sorin Mosut <mosutsorin@yahoo.com>
2020-04-10 22:43:49 +0300

    Homework lesson 7 exercise 1 completed

---
+++ lesson_7/mosut_sorin/hw_lesson7_decorators_mosut_sorin1.py
@@ -0,0 +1,38 @@
+'''
+Given the following function:
+
+def greet(name):
+    return "Greetings {}!".format(name)
+
+Create a decorator called uppercase that will uppercase the result
+
+@uppercase
+def greet(name):
+    return "Greetings {}!".format(name)
+
+print(greet("World"))
+>>> "GREETINGS WORLD!"
+
+
+def greet(name):
+    return "Greetings {}!".format(name)
+
+'''
+
+def uppercase(greet):
+
+    def uppercase_in(*args):
+
+        z = str.upper(greet(*args))
+
+        return z
+
+    return uppercase_in
+
+@uppercase
+def greet(name):
+    return "Greetings {}!".format(name)
+
+print(greet("World"))
+
+
