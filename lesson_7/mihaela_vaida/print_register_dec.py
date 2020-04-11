51f0247626621c35f0fb315c21f787bbfe7f5097
mihaelaGit <radoi_mihaela86@yahoo.com>
2020-04-11 18:19:54 +0300

    Lesson7_updates

--- lesson_7/mihaela_vaida/print_register_dec.py
+++ lesson_7/mihaela_vaida/print_register_dec.py
@@ -1,21 +1,30 @@
-
 print_registry = []
+
+
 def register(fnc):
-    
-    
-    def print_register(*args):        
+    def print_register(arg):
         print_registry.append(fnc.__name__)
         print(print_registry)
-    return print_register   
-        
+
+    return print_register
 
 
 @register
 def greet(name):
     return "Greetings {}!".format(name)
 
+
 def say_hello(name):
     return "Hello {}!".format(name)
+
+
 @register
 def say_goodbye(name):
     return "Goodbye {}!".format(name)
+
+
+greet('dsdas')  # ['greet']
+print(say_hello('Vio'))  # 'Hello Vio!'
+say_goodbye('sads')  # ['greet', 'say_goodbye']
+say_goodbye('sads')
+greet('dsdas')
