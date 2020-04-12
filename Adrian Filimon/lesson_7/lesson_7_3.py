fdb174ea71e9e1d6cffc38d3894370c931bf28ff
AdrianFilimon1707 <62895552+AdrianFilimon1707@users.noreply.github.com>
2020-04-12 16:20:18 +0200

    Last update3

---
+++ Adrian Filimon/lesson_7/lesson_7_3.py
@@ -0,0 +1,32 @@
+# Given a set of print methods:
+# print_registry = []
+#
+#
+# def greet(name):
+#     return "Greetings {}!".format(name)
+#
+#
+# def say_hello(name):
+#     return "Hello {}!".format(name)
+#
+#
+# def say_goodbye(name):
+#     return "Goodbye {}!".format(name)
+# Create a decorator called register that will update a list called print_registry with all the decorated functions names.
+#
+# print_registry = []
+#
+# @register
+# def greet(name):
+#     return "Greetings {}!".format(name)
+#
+#
+# def say_hello(name):
+#     return "Hello {}!".format(name)
+#
+# @register
+# def say_goodbye(name):
+#     return "Goodbye {}!".format(name)
+#
+# print(print_registry)
+# >>> ['greet', 'say_goodbye']
