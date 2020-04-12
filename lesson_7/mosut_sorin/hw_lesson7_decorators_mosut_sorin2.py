233575e8ef55f0f0fba92f07056b848ec92dcdde
Sorin Mosut <mosutsorin@yahoo.com>
2020-04-12 21:54:27 +0300

    Homework lesson 7 decorators ex 2 fix

--- lesson_7/mosut_sorin/hw_lesson7_decorators_mosut_sorin2.py
+++ lesson_7/mosut_sorin/hw_lesson7_decorators_mosut_sorin2.py
@@ -6,14 +6,13 @@ Create a decorator called safe_divide that will output a message
 if the division cannot be performed, othervise it will return the result.
 '''
 
-
 def safe_divide(fnc):
 
     def verif_fnc(*args):
         if args[1] == 0:
             return 'divide by 0'
         else:
-            return f'{args[0] / args[1]:.2f}'
+            return args[0] / args[1]
 
     return verif_fnc
 
@@ -21,4 +20,6 @@ def safe_divide(fnc):
 def divide(first_number, second_number):
     return first_number / second_number
 
-print(divide(20, 5))
+x = divide(5,3)
+print(f'{x:.2f}')
+print(f'{x * 5:.2f}')
