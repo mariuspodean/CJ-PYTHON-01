c464f81241f1e60bd57fdc04af575e1a3b33dd67
AdrianFilimon1707 <62895552+AdrianFilimon1707@users.noreply.github.com>
2020-04-12 17:54:17 +0200

    Last update3

--- Adrian Filimon/lesson_7/lesson_7_3.py
+++ Adrian Filimon/lesson_7/lesson_7_3.py
@@ -13,20 +13,32 @@
 # def say_goodbye(name):
 #     return "Goodbye {}!".format(name)
 # Create a decorator called register that will update a list called print_registry with all the decorated functions names.
-#
-# print_registry = []
-#
-# @register
-# def greet(name):
-#     return "Greetings {}!".format(name)
-#
-#
-# def say_hello(name):
-#     return "Hello {}!".format(name)
-#
-# @register
-# def say_goodbye(name):
-#     return "Goodbye {}!".format(name)
-#
-# print(print_registry)
-# >>> ['greet', 'say_goodbye']
+
+print_registry = []
+
+
+def register(funct):
+    def inner_funct(*args):
+        return print_registry.append(funct.__name__)
+
+    return inner_funct
+
+
+@register
+def greet(name):
+    return "Greetings {}!".format(name)
+
+
+def say_hello(name):
+    return "Hello {}!".format(name)
+
+
+@register
+def say_goodbye(name):
+    return "Goodbye {}!".format(name)
+
+say_hello('Cristi')
+greet('Cordelia')
+say_goodbye('Hollywood')
+
+print(print_registry)
