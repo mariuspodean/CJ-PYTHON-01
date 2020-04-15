8c2686dc1cf6895031cfd213de3e1c39ac8b39ef
Edi <edy.sabou@gmail.com>
2020-04-15 16:54:41 +0300

    changed code

--- lesson_7/edmond_sabou/lesson7_1.py
+++ lesson_7/edmond_sabou/lesson7_1.py
@@ -1,23 +1,16 @@
-import random
-
-
 def uppsercase(func):
 
-    annotations = ["-", "*", "+", "-", "="]
-    annotate = random.choice(annotations)
-
     def formatting(*args):
-        print(annotate * 20)
-        print(func(*args).upper())
-        print(annotate * 20)
+
+        return func(*args).upper()
 
     return formatting
 
 
 @uppsercase
 def print_message(name):
-    return "  Greetings {}!".format(name)
+    return "Greetings {}!".format(name)
 
 
-print_message("World")
-
+msg = print_message("World")
+print(msg)
