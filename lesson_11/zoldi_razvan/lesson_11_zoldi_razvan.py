9cc6c4e016eca12be297b69edeb9b4b6368bb2a2
Razvan Zoldi <62897785+zoldirazvan@users.noreply.github.com>
2020-04-15 21:34:43 +0300

    Please add my lesson 11 homework

---
+++ lesson_11/zoldi_razvan/lesson_11_zoldi_razvan.py
@@ -0,0 +1,63 @@
+# Challenge!
+# Make StudentsCollection a sized collection. Work in progress
+
+from collections.abc import MutableMapping, Mapping, Iterable, Collection, Container, Sized
+
+
+class Student(object):
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def __repr__(self):
+        class_name = type(self).__name__
+        return '{} ({} {} - {}) [{}]'.format(
+            class_name, self.first_name,
+            self.last_name, self.age, id(self)
+        )
+
+    def __str__(self):
+        return '{} {}, {}'.format(
+            self.first_name, self.last_name, self.age)
+
+
+class StudentsCollection:
+
+    def __init__(self, students_list=None):
+        self._students = list(students_list) if students_list else []
+
+    def __iter__(self):
+        return iter(self._students)
+
+    def __getitem__(self, index):
+        return self._students[index]
+
+    # def __len__(self):
+    #     return self._students[index]
+
+    # def __contains__(self, ):
+    #     return self._students[index]
+
+
+students = [
+    Student('John', 'Doe', 19),
+    Student('Jack', 'Fluffy', 18),
+    Student('Matthew', 'Wu', 19),
+    Student('Heather', 'Rafferty', 19),
+    Student('Randall', 'Blackdall', 20),
+    Student('Marissa', 'Raynaud', 19),
+    Student('Marlo', 'Ranbot', 19)
+]
+
+stud_collection = StudentsCollection(students)
+
+print(type(stud_collection))
+
+abcs = [MutableMapping, Mapping, Iterable, Collection, Container, Sized]
+for abc in abcs:
+    print(
+        'stud_collection is a {}: {}'.format(
+            abc.__name__, isinstance(stud_collection, abc))
+    )
