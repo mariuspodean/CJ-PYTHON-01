1c208bf7a1fb369fa71f546ed7a6b836e86ff16e
AlexRoman84 <vladroman84@yahoo.com>
2020-04-16 17:40:37 +0300

    Formatting the code and clearing a few errors ....

--- lesson_9/alex_roman/lesson_9_roman_alex.py
+++ lesson_9/alex_roman/lesson_9_roman_alex.py
@@ -1,10 +1,12 @@
-# Build two classes of your choice that can model a real-life example. The class needs to meet the following requirements:
+# Build two classes of your choice that can model a real-life example. The class needs to meet the following
+# requirements:
 
-    # at least 5 attributes each
-    # at least 2 methods each
-    # one class to inherit from another
+# at least 5 attributes each
+# at least 2 methods each
+# one class to inherit from another
 
-# As a demonstration create at least 5 instances of one class (preferably the child class) and call all the methods it holds
+# As a demonstration create at least 5 instances of one class (preferably the child class) and call all the methods
+# it holds
 
 class Client:
     no_of_clients = 0
@@ -27,39 +29,39 @@ class Client:
 
 
 new_client_1 = "John.Doe.USA.(001)2222-2222.HoneyWell"
-new_client_2 = "Jane.Doe.Canada.(002)3333-3333.Resor"
+new_client_2 = "Jane.Doe.Canada.(002)3333-3333.Regor"
 
 # Creating and checking client_1 form a string
 client_1 = Client.from_string(new_client_1)
-print(client_1.__dict__)
+print(vars(client_1))
 
 # Creating and checking client_2 form a string
 client_2 = Client.from_string(new_client_2)
-print(client_2.__dict__)
+print(vars(client_2))
 
 # Creating and checking client_3
 client_3 = Client("Joe", "Smith", "USA", "(001)4444-4444", "HoneyWell")
-print(client_3.__dict__)
+print(vars(client_3))
 
 
 class Gold_Client(Client):
     def __init__(self, first_name, last_name, country, telephone, company, discount):
-        super().__init__(self, first_name, last_name, country, telephone)
         self.discount = discount
+        super().__init__(first_name, last_name, country, telephone, company)
 
     def gold_client_discount(self):
-        return ('{} %'.format(self.discount))
+        return '{} %'.format(self.discount)
 
     def increase(self):
-        if int(self.discount)<=60:
+        if int(self.discount) <= 60:
             self.discount = int(self.discount) + 10
-            return(self.discount)
+            return self.discount
         else:
-            return(print("Discount is already at maximum"))
-
-
+            return print("Discount is already at maximum")
 
 
 client_5 = Gold_Client("Jake", "fisher", "USA", "(001)4444-4444", "J.Walker", "20")
+
 print(client_5.gold_client_discount())
+
 client_5.increase()
