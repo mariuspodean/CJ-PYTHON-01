38e857c4f6d1f70746addfc71529f7598d844b18
fm1hai <farcasmmihai@gmail.com>
2020-04-26 12:10:06 +0300

    homework #12 with some improvements

--- lesson_12/mihai_farcas/homework_lesson_12.py
+++ lesson_12/mihai_farcas/homework_lesson_12.py
@@ -13,13 +13,47 @@ class CrayonsBox(MutableSequence):
         return self._crayons[index]
 
     def __delitem__(self, key):
-        return self._crayons[key]
+        del (self._crayons[key])
 
-    def __setitem__(self, key, value):
-        pass
+    def __setitem__(self, index, value):
+        return self._crayons[index]== value
 
-    def insert(self):
-        pass
+    def insert(self,index,value):
+        return self._crayons.insert(index,value)
+
+def display_crayons_box_elements():
+    for i in range(len(crayons_box)):
+        print(crayons_box[i])
+def title(title_name):
+    print('_' * 20, '{} {}'.format(title_name, ('_' * 20)))
 
 crayons = 'White Yellow Blue Red Green Black Brown'.split()
 crayons_box = CrayonsBox(crayons)
+
+# print('_'*20,'{} {}'.format('len() exemple',('_'*20)))
+title('len() exemple')
+print('{} is the initial len() '.format(crayons_box.__len__()))
+display_crayons_box_elements()
+
+# print('_'*20,'{} {}\n'.format('insert() exemple',('_'*20)))
+title('insert() exemple')
+crayons_box.insert(3,'Element on the third position')
+print(crayons_box[3])
+print('insert the following on 3 position : {}'.format(crayons_box[3]))
+display_crayons_box_elements()
+print('{} is the len() after the insert of {}'.format(crayons_box.__len__(),crayons_box[3]))
+
+
+# print('_'*20,'{} {}\n'.format('setitem() exemple',('_'*20)))
+title('setitem() exemple')
+crayons_box.__setitem__(2,'Gigel')
+print(crayons_box[2])
+display_crayons_box_elements()
+
+# print('_'*20,'{} {}\n'.format('delitem() exemple for crayons_box[2]',('_'*20)))
+title('delitem() exemple for crayons_box[2]')
+display_crayons_box_elements()
+print('{} : is the element we want to delete and it is on crayons_box[2] '.format(crayons_box[2]))
+crayons_box.__delitem__(2)
+print('{} : is the new element on the crayons_boc[2]'.format(crayons_box[2]))
+display_crayons_box_elements()
