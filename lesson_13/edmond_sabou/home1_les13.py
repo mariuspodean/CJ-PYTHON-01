4907ee36580bdae6ce6acfa8a23fb758e2c03c64
Edi <edy.sabou@gmail.com>
2020-05-13 21:32:45 +0300

    updated classes

--- lesson_13/edmond_sabou/home1_les13.py
+++ lesson_13/edmond_sabou/home1_les13.py
@@ -1,14 +1,8 @@
 from math import sqrt
 
 
-class Perimeter:
+class Polygon:
 
-    def perimeter(self):
-        class_name = type(self).__name__
-        return f'The perimeter for {class_name} is {sum(self.sides)}.\n'
-
-
-class Polygon(Perimeter):
     def __init__(self, *args):
         self.sides = args
 
@@ -21,7 +15,30 @@ class Polygon(Perimeter):
             print(f"Side {side_index} with length: {length}.\n")
 
 
-class Triangle(Polygon):
+class Square(Polygon):
+
+    def __init__(self, *args):
+        super().__init__(*args)
+
+    def area(self):
+        class_name = type(self).__name__
+        side, *_ = self.sides
+        return side ** 2
+
+    @classmethod
+    def from_area(cls, sq):
+        sq = sqrt(sq)
+        return sq
+
+
+class Perimeter:
+
+    def perimeter(self):
+        class_name = type(self).__name__
+        return f'The perimeter for {class_name} is {sum(self.sides)}.\n'
+
+
+class Triangle(Polygon, Perimeter):
     # `s(s-a)(s-b)(s-c) ** 0.5` where s = `(a+b+c) / 2`
     def __init__(self, *args):
         super().__init__(*args)
@@ -33,23 +50,8 @@ class Triangle(Polygon):
         return f"The {class_name}'s area is {s * (s - a) * (s - b) * (s - c) ** 0.5}."
 
 
-class Square(Polygon):
-    def __init__(self, *args):
-        super().__init__(*args)
-
-    def area(self):
-        class_name = type(self).__name__
-        side, *_ = self.sides
-        return f"The {class_name}'s area is {side ** 2}."
-
-    def from_area(self):
-        class_name = type(self).__name__
-        sq = sqrt(self)
-        return f"The side length for is {sq}.\n"
-
-
 p = Polygon(3, 4, 5, 5, 7, 8, 9)
-print(p.perimeter())
+
 
 t = Triangle(3, 4, 5)
 print(t.area())
@@ -57,7 +59,6 @@ print(t.perimeter())
 
 s = Square(5)
 print(s.area())
-print(s.perimeter())
 
 sq = Square.from_area(25)
 print(sq)
