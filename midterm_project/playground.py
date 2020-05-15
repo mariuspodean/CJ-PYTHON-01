5e9e1cec8de556b3fb48642e66fc9a3a1916fa59
Edi <edy.sabou@gmail.com>
2020-05-15 22:05:46 +0300

    add

--- midterm_project/playground.py
+++ midterm_project/playground.py
@@ -1,18 +1,38 @@
 from shopping_list_archive import *
 
-mac_and_cheese = Recipe("Famous Mac and cheese", mac_and_cheese_ingredients)
-mac_test = Recipe("test", mac_and_cheese_ingredients)
-print(mac_and_cheese)
+annotations = ["-", "*", "+", "-"]
+annotate = random.choice(annotations) * 25
 
-ingredients = list(mac_and_cheese_ingredients.keys())
-print(ingredients)
+mac_and_cheese_ingredients = {
+    'macaroni': 1,
+    'cheese': 0.5,
+    'butter': 1,
+    'milk': 1
+}
 
-i = RecipeBox.pick("asd")
-print(i)
+pizza_ingredients = {
+    'flour': 1,
+    'water': 1,
+    'yeast': 0.5,
+    'cheese': 2,
+    'pepperoni': 1,
+    'sauce': 2
+}
 
-# A new recipe can be created starting from a
-# name and dictionary of ingredients and quantities.
+burger_ingredients = {
+    'bun': 1,
+    'beef': 1,
+    'mustard': 0.5,
+    'ketchup': 0.5,
+    "onion": 1
+}
 
-a = Recipe('test', {'unt': 1, 'marmelada': 23, 'oua': 3, 'cafea': 4})
-if "milk" in fridge:
-    print('yap')
+
+mac_and_cheese = Recipe("Famous Mac and Cheese", mac_and_cheese_ingredients)
+burger = Recipe("Burger Lalala", burger_ingredients)
+pizza = Recipe("Pizza Plm", pizza_ingredients)
+
+r = RecipeBox()
+print(r.add(burger))
+print(r.add(mac_and_cheese))
+print(r.add(pizza))
