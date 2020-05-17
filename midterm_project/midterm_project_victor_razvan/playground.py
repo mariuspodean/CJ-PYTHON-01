53df7a50db64d39d5e041a1fc495bfb9d5adf0b6
Victor <62120496+victorpop84@users.noreply.github.com>
2020-05-17 12:38:34 +0300

    please add our midterm project

---
+++ midterm_project/midterm_project_victor_razvan/playground.py
@@ -0,0 +1,60 @@
+from shopping_list import *  # thanks to Mihaela for the recipes
+
+# mac_and_cheese = Recipe('Mac and cheese', {'macaroni': 3, 'egg': 3, 'milk': 0.5, 'cheese': 1, 'sugar':3} )
+# mac_and_cheese.display()
+# cheesecake = Recipe('Cheesecake', {'flour': 1, 'milk': 1, 'egg': 3, 'cocoa': 1, 'sugar': 1, 'strawberry': 2})
+# cheesecake.display()
+# banana_bread = Recipe('Banana bread', {'flour': 1, 'yogourt': 1, 'banana': 2, 'honey': 1, 'cocoa':1, 'sugar':1})
+# banana_bread.display()
+fries = Recipe('Fries', {'potato': 1, 'oil': 1, 'salt': 1, 'cheese': 1})
+# print(fries)
+# print(fries.values())
+# fries.display()
+# home_made_chocolate = Recipe('Home made chocolate', {'milk': 2, 'cocoa': 1, 'sugar': 2, 'vanilla': 1, 'nut': 1})
+# print(mac_and_cheese)
+# print(fries)
+
+# recipesbox = RecipesBox(cheesecake, banana_bread, home_made_chocolate, mac_and_cheese)
+# recipesbox = RecipesBox(cheesecake)
+# recipesbox.add_recipe(fries)
+# print(recipesbox)
+# recipesbox.delete_recipe(home_made_chocolate)
+# print(recipesbox)
+# print(recipesbox.pick())
+#
+# print(recipesbox.pick(fries))
+#
+# for i in range(len(recipesbox.recipe)):
+#     print(recipesbox.recipe[i].name)
+#
+# recipesbox.add_recipe(recipesbox.pick())
+# recipesbox.delete_recipe(recipesbox.pick())
+# recipesbox.add_recipe(recipesbox.pick(cheesecake))
+# recipesbox.add_recipe(recipesbox.pick())
+
+fridge = Fridge({'milk': 4, 'potato': 3, 'egg': 2, 'banana': 10, 'sugar': 3, 'macaroni': 2, 'cocoa': 2})
+# print(fridge)
+# fridge.check_ingredient('milk')
+# ingredients_in_the_fridge = fridge.ingredients
+# ingredients_in_the_fridge['butter'] = 3
+# print(fridge)
+# fridge.remove_ingredient('milk', 4)
+# fridge.remove_ingredient('milk', 4)
+#
+# recipes_box= recipesbox.recipe
+# for recipe in recipes_box:
+#     print('Recipesbox contains: ', recipe.name)
+
+print(fridge.check_recipe(fries))
+
+# fridge.check_recipe(banana_bread)
+#
+# fridge.check_recipe(cheesecake)
+#
+# fridge.check_recipe(mac_and_cheese)
+# print(recipesbox)
+
+fridge = Fridge({'milk': 4, 'potato': 3, 'egg': 2, 'banana': 10, 'sugar': 3, 'macaroni': 2, 'cocoa': 2})
+# print(fridge)
+# print(fridge.check_ingredient('banana'))
+# print(fridge.check_recipe(fries))
